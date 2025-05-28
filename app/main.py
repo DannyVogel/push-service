f9c1c9d3c5bf575069c145d676b026e62b7d55ac
Danny <danny.hj.vogel@gmail.com>
2025-05-28 23:39:36 +0200

    Enhance CORS middleware configuration by allowing all origins

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
